de4bf38763b7005dc3d742d9194d52bdc7ed792e
Nigel Alford <nigel@callrail.com>
2019-05-21 10:00:08 -0400

    refactoring boilerplate code

--- index.js
+++ index.js
@@ -1,76 +1,4 @@
-/**
- * Define a function for initiating a conversation on installation
- * With custom integrations, we don't have a way to find out who installed us, so we can't message them :(
- */
-
-function onInstallation(bot, installer) {
-  if (installer) {
-    bot.startPrivateConversation({ user: installer }, function(err, convo) {
-      if (err) {
-        console.log(err);
-      } else {
-        convo.say('I am a bot that has just joined your team');
-        convo.say(
-          'You must now /invite me to a channel so that I can be of use!'
-        );
-      }
-    });
-  }
-}
-
-/**
- * Configure the persistence options
- */
-
-// Load process.env values from .env file
-require('dotenv').config();
-
-var config = {};
-if (process.env.MONGOLAB_URI) {
-  var BotkitStorage = require('botkit-storage-firestore');
-  config = {
-    storage: BotkitStorage({
-      mongoUri: process.env.MONGOLAB_URI,
-      tables: ['coffee']
-    })
-  };
-} else {
-  config = {
-    json_file_store: process.env.TOKEN
-      ? './db_slack_bot_ci/'
-      : './db_slack_bot_a/' //use a different name if an app or CI
-  };
-}
-
-/**
- * Are being run as an app or a custom integration? The initialization will differ, depending
- */
-
-if (process.env.TOKEN || process.env.SLACK_TOKEN) {
-  //Treat this as a custom integration
-  var customIntegration = require('./lib/custom_integrations');
-  var token = process.env.TOKEN ? process.env.TOKEN : process.env.SLACK_TOKEN;
-  var controller = customIntegration.configure(token, config, onInstallation);
-} else if (
-  process.env.CLIENT_ID &&
-  process.env.CLIENT_SECRET &&
-  process.env.PORT
-) {
-  //Treat this as an app
-  var app = require('./lib/apps');
-  var controller = app.configure(
-    process.env.PORT,
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    config,
-    onInstallation
-  );
-} else {
-  console.log(
-    'Error: If this is a custom integration, please specify TOKEN in the environment. If this is an app, please specify CLIENTID, CLIENTSECRET, and PORT in the environment'
-  );
-  process.exit(1);
-}
+var controller = require('./lib/apps').pantryBot();
 
 /**
 TODO: Move off RTM
@@ -134,10 +62,8 @@ controller.hears('menu', mention, (bot, msg) => {
   var end = new Date();
   end.setHours(23, 59, 59, 999);
 
-  controller.storage.coffee.find(
-    { brew: { $gte: start, $lt: end } },
-    (error, coffee) => {
-      if (error) return console.error(error);
+  controller.storage.coffee.all((error, coffee) => {
+      if (error) return console.error({error});
       if (coffee.length > 0) {
         coffee.forEach(({ name, roaster, url }) => {
           const u = url || 'http://www.google.com';
